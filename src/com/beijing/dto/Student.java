7bd91b333c35d657b30b897ca3bf051af8d9583b
SJH <486555066@qq.com>
2019-06-21 09:38:01 +0800

    新写了一个Student类

---
+++ src/com/beijing/dto/Student.java
@@ -0,0 +1,13 @@
+package com.beijing.dto;
+
+/**
+ * @author shenjianhua
+ * @create 2019/6/21 9:35
+ * @package com.beijing.dto
+ */
+
+public class Student {
+    private int age;
+    private String name;
+    private String address;
+}
