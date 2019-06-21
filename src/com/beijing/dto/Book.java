bce52c635561a19c33997d03d092fa422672688e
SJH <486555066@qq.com>
2019-06-21 10:13:08 +0800

    新建了Book类和Car类

---
+++ src/com/beijing/dto/Book.java
@@ -0,0 +1,10 @@
+package com.beijing.dto;
+
+/**
+ * @author shenjianhua
+ * @create 2019/6/21 10:06
+ * @package com.beijing.dto
+ */
+
+public class Book {
+}
---
+++ src/com/beijing/dto/Car.java
@@ -0,0 +1,13 @@
+package com.beijing.dto;
+
+/**
+ * @author shenjianhua
+ * @create 2019/6/21 10:05
+ * @package com.beijing.dto
+ */
+
+public class Car {
+    private int id;
+    private String color;
+    private String name;
+}
